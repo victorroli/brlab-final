e02558fadbecf69badc4c89bb33e5dcfba713545
Victor Oliveira <victorfelipe1608@gmail.com>
2019-07-25 23:36:54 -0300

    Inserção do modal na tela de Login

--- src/views/usuario/Login.vue
+++ src/views/usuario/Login.vue
@@ -16,43 +16,98 @@
         <router-link :to="{name: 'register-user'}">Criar nova conta</router-link>
       </p>
     </div>
+
+    <b-modal ref="my-modal" hide-footer>
+      <div class="d-block text-center">
+        <slot>
+          <h1>{{resposta}}</h1>
+        </slot>
+      </div>
+      <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Fechar</b-button>
+    </b-modal>
   </section>
 </template>
 
 <script>
 // import { mapFields } from "@/helpers/mapFields.js";
 import { mapState } from "vuex";
+// import ModalMensagem from "@/components/ModalMensagem.vue";
 export default {
   name: "Login",
+  components: {
+    // mensagem: ModalMensagem
+  },
   data() {
     return {
       dadosLogin: {
         email: "",
         senha: ""
-      }
+      },
+      resposta: "",
+      boxConfirmation: ""
     };
   },
 
   methods: {
     logar() {
-      this.$store.dispatch("login", this.dadosLogin);
-      console.log("Login: ", this.$store.state.login);
+      if (this.verificaCampos()) {
+        this.$store.dispatch("login", this.dadosLogin);
+        this.resposta = "Logado com sucesso!";
+        //   // console.log("Login: ", this.$store.state.login);
+      } else this.resposta = "Credenciais erradas";
+      this.$bvModal
+        .msgBoxConfirm(this.resposta, {
+          size: "md",
+          buttonSize: "md",
+          // okTitle: "Ok",
+          // okOnly: true,
+          cancelOnly: true,
+          // cancelVariant: true,
+          // footerClass: "p-3",
+          // hideHeaderClose: true,
+          centered: true
+        })
+        .then(resposta => {
+          // this.boxOne = value;
+          if (this.login) {
+            if (resposta == null || resposta) {
+              this.$router.push({ path: "/" });
+            }
+          }
+        })
+        .catch(err => {
+          // An error occurred
+        });
+    },
+    verificaCampos() {
+      if (!this.dadosLogin.email) {
+        console.log("Email inválido...");
+        return false;
+      }
+      if (!this.dadosLogin.senha) {
+        console.log("Senha inválida...");
+        return false;
+      }
+      return true;
+    },
+    hideModal() {
+      this.$refs["my-modal"].hide();
     }
   },
   watch: {
-    login(novo_valor, antigo) {
-      // console.log(novo_valor);
+    login(novo_valor) {
       if (novo_valor) {
-        alert("Logado com sucesso!!!");
-        this.$router.push({ path: "/" });
+        // this.$refs["my-modal"].show();
       }
-      // else {
-      //   alert("Credenciais erradas, tente novamente!");
-      // }
     }
   },
   computed: {
-    ...mapState(["login"])
+    ...mapState(["login"]),
+    respostaMensagem() {
+      if (this.verificaCampos()) {
+        return true;
+      } else return false;
+    }
   }
 };
 </script>
