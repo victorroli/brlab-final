76a5decb5aba25cb08fdc83fadbb659752ec1233
Victor Oliveira <victorfelipe1608@gmail.com>
2019-09-07 19:09:49 -0300

    Chamada do modal para edição do Laboratório

--- src/views/laboratorio/ListaLaboratoriosCadastrados.vue
+++ src/views/laboratorio/ListaLaboratoriosCadastrados.vue
@@ -1,16 +1,24 @@
 <template>
   <section>
     <h1>Laboratórios Cadastrados</h1>
+
     <div id="laboratorios" v-if="laboratorios.length > 0">
       <b-table striped hover :items="laboratorios" :fields="fields" class="text-center">
         <template slot="opcoes" slot-scope="row">
-          <b-button class="editar" @click="editar(row.item)">Editar</b-button>
-          <b-button class="excluir" @click="excluirInstituicao(row.item)">Excluir</b-button>
+          <b-button class="editar" @click="editar(row.item)">
+            <font-awesome-icon icon="edit" />Editar
+          </b-button>
+          <b-button class="excluir" @click="excluir(row.item)">
+            <font-awesome-icon icon="trash" />Excluir
+          </b-button>
         </template>
       </b-table>
       <div class="group-button">
-        <router-link tag="b-button" class="button" :to="{name: 'novo-laboratorio'}">Novo Laboratório</router-link>
+        <router-link tag="b-button" class="button" :to="{name: 'novo-laboratorio'}">
+          <font-awesome-icon icon="plus-circle" />Novo Laboratório
+        </router-link>
       </div>
+      <modal-laboratorio ref="modal"></modal-laboratorio>
     </div>
     <div v-else>
       <h3>Nenhum registrado encontrado!!!</h3>
@@ -19,9 +27,13 @@
 </template>
 
 <script>
+import ModalLaboratorio from "@/components/ModalLaboratorio.vue";
 import { api } from "@/services.js";
 export default {
   name: "LaboratoriosCadastrados",
+  components: {
+    "modal-laboratorio": ModalLaboratorio
+  },
   data() {
     return {
       laboratorios: [],
@@ -50,11 +62,21 @@ export default {
   methods: {
     buscaLaboratorios() {
       api.get("/labs/").then(response => {
-        console.log("Resposta obtida: ", response.data);
         let aLabs = response.data.filter(laboratorio => {
           return laboratorio.status == 2;
         });
         this.laboratorios = [...aLabs];
+      });
+    },
+    editar(item) {
+      this.$refs.modal.recebeValoresLaboratorios(item);
+    },
+    excluir(item) {
+      api.delete(`/laboratorios/${instituicao.id}`).then(response => {
+        if (response.data.status == 200) {
+          this.confirmExclusao(instituicao.nome);
+          this.buscaInstituicoes();
+        }
       });
     }
   },
