882ee9b948b91466f6bc271dc48a2428e93f6321
Victor Oliveira <victorfelipe1608@gmail.com>
2019-11-22 18:17:36 -0300

    Filtro por laboratório na tela de reserva

--- src/views/Reservas.vue
+++ src/views/Reservas.vue
@@ -1,8 +1,25 @@
 <template>
   <section>
     <h1>Reservas</h1>
-    <div id="reservas">
-      <b-table striped hover :items="reservas"></b-table>
+    <div id="box-laboratorio">
+      <b-form-select id="select-laboratorio" v-model="laboratorio" :options="lista_laboratorios"></b-form-select>
+    </div>
+
+    <div id="reservas" v-if="reservas.length">
+      <b-table
+        v-if="this.$store.state.usuario.papel_id == 1"
+        striped
+        hover
+        :items="reservas"
+        :fields="fields_adm"
+      ></b-table>
+      <b-table v-else striped hover :items="reservas" :fields="fields"></b-table>
+    </div>
+    <div class="texto-reserva" v-else-if="laboratorio != 0">
+      <p>Nenhuma reserva efetuada para este laboratório...</p>
+    </div>
+    <div v-else class="texto-reserva">
+      <p>Selecione um laboratório...</p>
     </div>
   </section>
 </template>
@@ -12,36 +29,93 @@ import { api } from "@/services.js";
 export default {
   name: "reservas",
   data() {
-    return { reservas: [] };
+    return {
+      reservas: [],
+      fields_adm: [
+        {
+          key: "usuario",
+          label: "Usuário"
+        },
+        {
+          key: "titulo",
+          label: "Título"
+        },
+        {
+          key: "horario_inicio",
+          label: "Horário Início"
+        },
+        {
+          key: "horario_termino",
+          label: "Horário Término"
+        },
+        {
+          key: "observacao",
+          label: "Observação"
+        }
+      ],
+      fields: [
+        {
+          key: "titulo",
+          label: "Título"
+        },
+        {
+          key: "horario_inicio",
+          label: "Horário Início"
+        },
+        {
+          key: "horario_termino",
+          label: "Horário Término"
+        },
+        {
+          key: "observacao",
+          label: "Observação"
+        }
+      ],
+      lista_laboratorios: [{ value: 0, text: "Selecione um laboratório" }],
+      laboratorio: 0
+    };
   },
   created() {
-    this.listaReservas();
+    this.listaLaboratorios();
+  },
+  watch: {
+    laboratorio: function(valor, old) {
+      if (valor) {
+        this.listaReservas(valor);
+      } else this.laboratorio = valor;
+    }
   },
   methods: {
-    listaReservas() {
-      api
-        .get(`/agendamento/usuario/${this.$store.state.usuario.id}`, {})
-        .then(response => {
-          console.log("Resposta obtida: ", response.data);
-          // let horarioInicio = this.converteDate(reserva.periodo_inicio);
-          let reservasUsuario = [];
-          response.data.forEach(reserva => {
-            let oReserva = new Object({
-              horario_inicio: this.converteDate(reserva.periodo_inicio),
-              horario_termino: this.converteDate(reserva.periodo_fim),
-              titulo: "Horário reservado",
-              observacao: reserva.observacao
+    listaLaboratorios() {
+      api.get(`/labs/`, {}).then(response => {
+        if (response.status == 200) {
+          response.data.forEach(laboratorio => {
+            this.lista_laboratorios.push({
+              value: laboratorio.id,
+              text: laboratorio.name
             });
-            // Trecho a ser verificado posteriormente para checar o status da reserva no momento
-            // let horaInicio = new Date(horarioInicio);
-            // let horarioAtual = new Date();
-            // console.log("Horário início: ", horarioInicio);
-            // console.log("Horário atual: ", horarioAtual);
-
-            // console.log("Reserva efetuada: ", oReserva);
-            reservasUsuario.push(oReserva);
           });
-
+        }
+      });
+    },
+    listaReservas(id_laboratorio) {
+      api
+        .get(`/agendamento/laboratorio/${id_laboratorio}`, {})
+        .then(response => {
+          console.log("Resposta obtida: ", response);
+          let reservasUsuario = [];
+          if (response.data.length > 1) {
+            response.data.forEach(reserva => {
+              let oReserva = new Object({
+                usuario: reserva.usuario,
+                horario_inicio: this.converteDate(reserva.periodo_inicio),
+                horario_termino: this.converteDate(reserva.periodo_fim),
+                titulo: "Horário reservado",
+                observacao: reserva.observacao
+              });
+              reservasUsuario.push(oReserva);
+            });
+          }
           this.reservas = [...reservasUsuario];
         });
     },
@@ -65,14 +139,13 @@ export default {
       hora = this.checValores(dataConvertida.getHours());
       minutos = this.checValores(dataConvertida.getMinutes());
 
-      console.log(`${ano}-${mes}-${dia} ${hora}:${minutos}`);
-      return `${ano}-${mes}-${dia} ${hora}:${minutos}`;
+      return `${hora}:${minutos} ${dia}-${mes}-${ano}`;
     },
     checValores(valor, mes) {
       let valorFinal = valor;
       if (valor < 10) {
         if (mes) valorFinal = valor + 1;
-        return `0${valorFinal}`;
+        if (valorFinal < 10) valorFinal = `0${valorFinal}`;
       }
       return valorFinal;
     }
@@ -85,5 +158,19 @@ export default {
   max-width: 80%;
   margin-left: 10%;
   margin-right: 10%;
+}
+
+#box-laboratorio {
+  text-align: center;
+}
+
+#select-laboratorio {
+  width: 50%;
+  margin-bottom: 40px;
+}
+
+.texto-reserva {
+  text-align: center;
+  font-size: 1.5rem;
 }
 </style>
