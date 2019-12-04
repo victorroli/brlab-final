fa72b1e08993c1a3b51f5d18b2334c864d6c8ac4
Victor Oliveira <victorfelipe1608@gmail.com>
2019-12-03 21:31:32 -0300

    Correção do posicionamento das descrições na tela do laboratório

--- src/views/laboratorio/DadosLaboratorio.vue
+++ src/views/laboratorio/DadosLaboratorio.vue
@@ -1,15 +1,21 @@
 <template>
   <b-container class="lab-container">
-    <b-row>
+    <b-row class="linha-1">
       <b-col class="lab-selecionado">
         <div class="lab-img">
           <img src="../../assets/lab.svg" alt />
         </div>
         <div class="lab-info">
           <h3>{{lab_selecionado.name}}</h3>
-          <p>{{lab_selecionado.description}}</p>
+          <p>
+            <b>Descrição:</b>
+            {{lab_selecionado.description}}
+          </p>
           <div class="tempo-experimento">
-            <p>Tempo de Experimentação: {{lab_selecionado.tempo}} minutos</p>
+            <p>
+              <b>Tempo de Experimentação:</b>
+              {{lab_selecionado.tempo}} minutos
+            </p>
           </div>
           <b-row>
             <b-col class="text-center btn-div" v-if="true">
@@ -33,19 +39,21 @@
         </div>
       </b-col>
     </b-row>
-    <b-row>
-      <b-col class="text-left" v-if="equipamentos != ''">
-        <h4>Equipamentos Disponíveis</h4>
+    <b-row class="linha-2">
+      <b-col class="text-center" v-if="equipamentos != ''">
+        <h3>Equipamentos Disponíveis</h3>
         <ul>
-          <li
-            v-for="(equipamentos, index) in equipamentos"
-            :key="index"
-          >{{equipamentos.nome}}: {{equipamentos.descricao}}</li>
+          <li v-for="(equipamentos, index) in equipamentos" :key="index">
+            <b>
+              <u>{{equipamentos.nome}}</u>:
+            </b>
+            {{equipamentos.descricao}}
+          </li>
         </ul>
       </b-col>
     </b-row>
     <hr />
-    <b-row>
+    <b-row class="linha-3">
       <b-col class="text-center">
         <h3>Últimos Experimentos</h3>
         <p>Nenhum experimento realizado</p>
@@ -117,7 +125,11 @@ h3 {
   color: #84f;
   margin-top: 40px;
   font-size: 1.5rem;
-  text-align: center;
+}
+
+.linha-2 h3 {
+  text-align: left;
+  position: static;
 }
 
 /* .lab-container {
@@ -170,13 +182,17 @@ hr {
   margin-left: 30%;
   color: #84f;
 }
-.tempo-experimento p {
+/* .tempo-experimento p {
   padding: 20px;
   font-size: 17px;
-}
+} */
 .descricao-lab {
   font-size: 1.5rem;
   color: rgb(138, 103, 197);
   font-weight: 550;
+}
+.lab-info p {
+  text-align: center;
+  font-size: 1rem;
 }
 </style>
