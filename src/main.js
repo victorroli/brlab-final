bf47ec257ad6e58c10ef67a9c1a267cafa500162
Victor Oliveira <victorfelipe1608@gmail.com>
2019-12-06 12:20:14 -0300

    Inclusão de filters

--- src/main.js
+++ src/main.js
@@ -41,12 +41,17 @@ Vue.use(Datetime);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
-Vue.filter("converteData", valor => {
-  let date = new Date(valor);
-  let horas = date.getHours();
-  let minutos = date.getMinutes();
-  let segundos = date.getSeconds();
+Vue.filter("resultadoExperimento", (inicio, fim) => {
+  console.log("Inicio: ", inicio);
+  console.log("Fim: ", fim);
+  let date_ini = new Date(inicio);
+  let date_fim = new Date(fim);
+
+  let horas = date_ini.getHours() - date_fim.getHours();
+  let minutos = date_ini.getMinutes() - date_fim.getMinutes();
+  let segundos = date_ini.getSeconds() - date_fim.getSeconds();
   let tempoTotal = "";
+
   if (horas) {
     tempoTotal += ` Horas: ${horas}`;
   }
@@ -56,8 +61,34 @@ Vue.filter("converteData", valor => {
   if (segundos) {
     tempoTotal += ` Segundos: ${segundos}`;
   }
-
+  console.log("Tempo total: ", tempoTotal);
   return tempoTotal;
+});
+
+Vue.filter("formataData", valor => {
+  let data = new Date(valor);
+  let dia = data.getDate();
+  let mes = data.getMonth();
+  let ano = data.getFullYear();
+
+  if (dia < 9) {
+    dia = "0" + dia;
+  }
+
+  if (mes < 9) {
+    mes = "0" + mes;
+  }
+
+  return `${dia}/${mes + 2}/${ano}`;
+});
+
+Vue.filter("formataHorario", valor => {
+  let data = new Date(valor);
+  let horas = data.getHours();
+  let minutos = data.getMinutes();
+  let segundos = data.getSeconds();
+
+  return `${horas}:${minutos}`;
 });
 
 new Vue({
